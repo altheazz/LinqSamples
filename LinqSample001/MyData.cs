6a9468d74b0483266c08e462535fe0945cb667e9
althea <azhong531@gmail.com>
2023-02-15 10:52:23 +0800

    加入專案檔案。

---
+++ LinqSample001/MyData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSample001
+{
+    internal class MyData //類別
+    {
+        public string Name //屬性
+        { get; set; }
+        public int Age //屬性
+        { get; set; }
+    }
+}
---
+++ LinqSample001/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSample001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MyData> list = CreateList();
+        }
+        static  List<MyData> CreateList()
+        {
+            return new List<MyData>()
+            {
+                new MyData{ Name = "Bill", Age = 47},
+                new MyData{ Name = "Jhon", Age = 37},
+                new MyData{ Name = "Tom", Age = 48},
+                new MyData{ Name = "David", Age = 36},
+                new MyData{ Name = "Bill", Age = 35},
+            };
+        }
+    }
+}
